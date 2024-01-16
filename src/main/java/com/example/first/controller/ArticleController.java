3a3ec853eebea9ab87fa5a0fe85d4e9879db6239
eun <ye90202@gmail.com>
2024-01-16 19:12:41 +0900

    디티오를 엔티티로 변환하고 엔티티를 디비에 저장

--- src/main/java/com/example/first/controller/ArticleController.java
+++ src/main/java/com/example/first/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.first.controller;
 import com.example.first.dto.ArticleForm;
 import com.example.first.entity.Article;
 import com.example.first.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j //로깅을 위한 어노테이션 문법
 public class ArticleController {
 
     @Autowired //스프링부트가 미리생성해놓은 객체를 가져다가 자동연결 = new 할 필요 x
@@ -20,17 +22,17 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
+        log.info(form.toString());//데이터가 로그로 찍힘
 
-
-        System.out.println(form.toString());
+        //System.out.println(form.toString()); 로깅으로 대체
 
         //1. dto를 엔티티로 변환 articles/new에서 받은 데이터를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
 
         //2. Repository에게 엔티티를 디비 안에 저장
         Article saved = articleRepository.save(article); //상속 받은 클래스에 save라는 메소드가 있음
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
---
+++ src/main/java/com/example/first/entity/Article.java
@@ -0,0 +1,27 @@
+package com.example.first.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor //생성자대신
+@ToString //스트링대신
+@Entity //디비가 해당 객체를 인식가능하게 함
+public class Article {
+    @Id
+    @GeneratedValue // 자동생성 어노테이션
+    private Long id; //기본키
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    public Article() {
+
+    }
+}
